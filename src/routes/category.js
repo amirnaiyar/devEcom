e4ba2f122b6d9e82f22352f606956aa94be2b4a8
Amir Khan <amirnaiyarkhan@gmail.com>
2024-11-03 20:56:59 +0530

    save change

--- src/routes/category.js
+++ src/routes/category.js
@@ -21,7 +21,6 @@ categoryRouter.post('/', async (req, res) => {
 categoryRouter.get('/', async (req, res) => {
     try {
         const categories = await Category.find();
-        console.log(categories, 'category');
         res.status(200).json(categories);
     } catch (error) {
         res.status(400).json({ message: 'Error fetching categories', error });
